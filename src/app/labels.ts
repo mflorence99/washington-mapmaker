c32888caa76257128a4876b2e1bf7bea3cb56a7c
Mark Florence <mflo999@gmail.com>
2021-06-27 13:03:01 -0400

    Fiddling with syles

--- src/app/labels.ts
+++ src/app/labels.ts
@@ -13,18 +13,45 @@ import { Component } from '@angular/core';
     }}"
   >
     <ng-container *ngFor="let lot of parcels.parcels.lots">
-      <ng-container *ngIf="lot.area >= 10 || geometry.profile !== 'washington'">
-        <ng-container *ngFor="let center of lot.centers">
-          <g *ngIf="geometry.point2xy(center) as xy">
-            <text [attr.x]="xy[0]" [attr.y]="xy[1]" text-anchor="middle">
+      <ng-container *ngFor="let center of lot.centers">
+        <g
+          *ngIf="geometry.point2xy(center) as xy"
+          [ngClass]="[geometry.profile, 'z' + geometry.zoom]"
+        >
+          <text [attr.x]="xy[0]" [attr.y]="xy[1]" text-anchor="middle">
+            <tspan [ngClass]="'a' + quantize(lot.area)" class="id">
               {{ lot.id }}
-            </text>
-          </g>
-        </ng-container>
+            </tspan>
+            <tspan
+              [attr.x]="xy[0]"
+              [attr.dy]="'1.1em'"
+              [ngClass]="'a' + quantize(lot.area)"
+              class="area"
+            >
+              {{ round(lot.area) }} ac
+            </tspan>
+          </text>
+        </g>
       </ng-container>
     </ng-container>
   </svg>`
 })
 export class LabelsComponent {
   constructor(public geometry: Geometry, public parcels: Parcels) {}
+
+  quantize(area: number): number {
+    if (area >= 500) return 500;
+    else if (area >= 100) return 100;
+    else if (area >= 50) return 50;
+    else if (area >= 25) return 25;
+    else if (area >= 10) return 10;
+    else if (area >= 5) return 5;
+    else if (area >= 2) return 2;
+    else if (area >= 1) return 1;
+    else return 0;
+  }
+
+  round(area: number): number {
+    return Math.round(area * 10) / 10;
+  }
 }
