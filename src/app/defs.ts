6cdb8dd74839f9d3495b5d4a4e18e36419818551
Mark Florence <mflo999@gmail.com>
2021-07-02 10:42:23 -0400

    Compromise of halftone: use only on z17

--- src/app/defs.ts
+++ src/app/defs.ts
@@ -13,12 +13,12 @@ import { Component } from '@angular/core';
         *ngFor="let usage of parcels.parcels.usages"
         [attr.id]="'halftone-u' + usage"
         patternUnits="userSpaceOnUse"
-        width="2"
-        height="2"
+        width="4"
+        height="4"
       >
         <g [attr.fill]="'rgba(var(--shade-u' + usage + ', 0.75)'">
-          <rect x="0" y="0" width="1" height="1" />
-          <rect x="1" y="1" width="1" height="1" />
+          <rect x="0" y="0" width="2" height="2" />
+          <rect x="2" y="2" width="2" height="2" />
         </g>
       </pattern>
     </defs>
