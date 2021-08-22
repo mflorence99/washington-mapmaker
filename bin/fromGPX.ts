192cd7edf404973aebd439b2938b06e8ed175708
Mark Florence <mflo999@gmail.com>
2021-08-22 05:26:08 -0400

    Lotline corrections

--- bin/fromGPX.ts
+++ bin/fromGPX.ts
@@ -1,3 +1,5 @@
+import { PARCELS } from '../src/app/parcel-data';
+
 import { hideBin } from 'yargs/helpers';
 import { parseStringPromise } from 'xml2js';
 import { readFileSync } from 'fs';
@@ -7,42 +9,53 @@ import yargs from 'yargs';
 
 // 👇 npm run fromGPX -- --lotID="11-39-0" --lotID="11-39-1" --lotID="11-39-2" --lotID="11-39-3" --out="11-39" --usage="110"
 
+// 👇 npm run fromGPX -- --lotID="11-27" --out="11-27" --usage="120"
+
 const argv = yargs(hideBin(process.argv)).argv;
+
+const lotIDs = Array.isArray(argv['lotID']) ? argv['lotID'] : [argv['lotID']];
+const usage = String(argv['usage'] ?? 110);
+
+const lotByID = PARCELS.lots.reduce((acc, lot) => {
+  acc[lot.id] = lot;
+  return acc;
+}, {});
 
 async function main(): Promise<void> {
   const parcels = [];
-  for (const lotID of argv['lotID']) {
+  for (const lotID of lotIDs) {
+    const lot = lotByID[lotID];
     const xml = readFileSync(`src/assets/data/${lotID}.gpx`);
     const obj = await parseStringPromise(xml);
     parcels.push({
-      address: '',
-      area: 0,
+      address: lot?.address ?? '',
+      area: lot?.area ?? 0,
       areas: null,
-      boundaries: [
-        obj.gpx.trk[0].trkseg[0].trkpt.map((obj) => ({
+      boundaries: obj.gpx.trk[0].trkseg.map((trkseg) => {
+        return trkseg.trkpt.map((obj) => ({
           lat: Number(obj.$.lat),
           lon: Number(obj.$.lon)
-        }))
-      ],
-      building$: 0,
-      callouts: [],
+        }));
+      }),
+      building$: lot?.building$ ?? 0,
+      callouts: lot?.callouts ?? [],
       centers: null,
-      cu$: 0,
+      cu$: lot?.cu$ ?? 0,
       elevations: null,
       id: lotID,
-      labels: [],
-      land$: 0,
+      labels: lot?.labels ?? [],
+      land$: lot?.land$ ?? 0,
       lengths: null,
       minWidths: null,
-      neighborhood: '',
+      neighborhood: lot?.neighborhood ?? '',
       orientations: null,
-      owner: '',
+      owner: lot?.owner ?? '',
       perimeters: null,
       sqarcities: null,
-      taxed$: 0,
-      usage: String(argv['usage']),
-      use: '',
-      zone: ''
+      taxed$: lot?.taxed ?? 0,
+      usage: lot?.usage ?? usage,
+      use: lot?.use ?? '',
+      zone: lot?.zone ?? ''
     });
   }
   writeFileSync(
